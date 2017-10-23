1c966414a7a2b3b12f86a2e4ef933f6540b8adeb
jelf <okxiaoliang4@gmail.com>
2017-10-23 17:58:15 +0800

    feat(*): 准备将demo页面改成用手风琴与list的组合表

---
+++ src/components/carousel/index.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+import Swiper from '../swiper/index.vue'
+
+export default {
+    components: {Swiper}
+}
+</script>
+
+<style>
+
+</style>
--- src/page/demo/index.vue
+++ src/page/demo/index.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+        <accordion>
+            <list></list>
+        </accordion>
         <router-link to="/demo/accordion">accordion</router-link>
         <router-link to="/demo/flex">flex</router-link>
         <router-link to="/demo/list">list</router-link>
@@ -10,7 +13,19 @@
 </template>
 
 <script>
+import Accordion from '../../components/accordion/index.vue'
+import { Brief, List, ListItem } from '../../components/list/index.js'
 
+export default {
+    components: {Accordion, Brief, List, ListItem},
+    data () {
+        return {
+            options: {
+
+            }
+        }
+    }
+}
 </script>
 
 <style>
