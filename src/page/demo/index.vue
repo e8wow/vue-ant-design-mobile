8b061686098b9c9cfde4b13682c86949c51d4589
jelf <okxiaoliang4@gmail.com>
2017-10-23 17:45:43 +0800

    refactor(router): 更改路由。将demo页面放入demo二级目录

---
+++ src/page/demo/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <router-link to="/demo/accordion">accordion</router-link>
+        <router-link to="/demo/flex">flex</router-link>
+        <router-link to="/demo/list">list</router-link>
+        <router-link to="/demo/swiper">swiper</router-link>
+        <router-link to="/demo/whiteSpace">white-space</router-link>
+        <router-link to="/demo/wingBlank">wing-blank</router-link>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
---
+++ src/router/demo/index.js
@@ -0,0 +1,36 @@
+const DemoRouter = (resolve) => { require(['../../page/demo.vue'], resolve) }
+const Index = (resolve) => { require(['../../page/demo/index.vue'], resolve) }
+
+const Accordion = (resolve) => { require(['../../components/accordion/demo/index.vue'], resolve) }
+const Flex = (resolve) => { require(['../../components/flex/demo/index.vue'], resolve) }
+const List = (resolve) => { require(['../../components/list/demo/index.vue'], resolve) }
+const Swiper = (resolve) => { require(['../../components/swiper/demo/index.vue'], resolve) }
+const WhiteSpace = (resolve) => { require(['../../components/white-space/demo/index.vue'], resolve) }
+const WingBlank = (resolve) => { require(['../../components/wing-blank/demo/index.vue'], resolve) }
+
+export default {
+    path: '/demo',
+    component: DemoRouter,
+    children: [{
+        path: '/',
+        component: Index
+    }, {
+        path: 'accordion',
+        component: Accordion
+    }, {
+        path: 'flex',
+        component: Flex
+    }, {
+        path: 'list',
+        component: List
+    }, {
+        path: 'swiper',
+        component: Swiper
+    }, {
+        path: 'whiteSpace',
+        component: WhiteSpace
+    }, {
+        path: 'WingBlank',
+        component: WingBlank
+    }]
+}
